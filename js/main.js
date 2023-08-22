e8eb5af34005ea621616927e382a6c62737f820d
Brianceasar <95188858+Brianceasar@users.noreply.github.com>
2023-08-22 23:55:17 +0300

    add animated circular team section

--- js/main.js
+++ js/main.js
@@ -328,3 +328,25 @@
 
 
 })(jQuery);
+
+
+/*----------------------------------------------------*/
+	/*	Team-section
+	------------------------------------------------------*/
+
+	// let imgBx = document.querySelectorAll('.imgBx');
+	// let contentBx = document.querySelectorAll('.contentBx');
+
+	// for(let i=0; i<imgBx.length; i++){
+	// 	imgBx[i].addEventListener('mouseover', function(){
+	// 		for(let i=0; i<contentBx.length;i++){
+	// 			contentBx[i].className = 'contentBx';
+	// 		}
+	// 		document.getElementById(this.dataset.id)
+	// 		.className='contentBx active';
+
+	// 		for(let i=0; i<imgBx.length;i++){
+	// 			imgBx[i].className = 'imgBx';
+	// 		}
+	// 	});
+	// }
