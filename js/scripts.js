73a50b7921f030863db0789e63267b944a7eef8c
Brianceasar <95188858+Brianceasar@users.noreply.github.com>
2024-04-04 11:28:33 +0300

    Close #34 Pre loader text changed One Content, One Story, One impact at a time

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,7 @@
 		// TYPEWRITER
 			$("#typewriter").typewriter({
 				prefix : "",
-				text : ["Please wait", "Still loading", "Almost done"],
+				text : ["One Content", "One Story", "One Impact at a Time"],
 				typeDelay : 100,
 				waitingTime : 1500,
 				blinkSpeed : 800
