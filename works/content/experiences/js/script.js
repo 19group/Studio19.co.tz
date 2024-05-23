c45770ce953236a789dd4e7996476b128179f9ea
Brianceasar <95188858+Brianceasar@users.noreply.github.com>
2024-05-23 20:57:43 +0300

    fixes #72 the applyFiltersFromURL function is properly called the filters are applied accurately when the page loads with URL parameters.

--- works/content/experiences/js/script.js
+++ works/content/experiences/js/script.js
@@ -13,50 +13,29 @@ const archiveElements = [];
 function createArchiveElement(item) {
     const archiveElement = document.createElement('div');
     archiveElement.className = 'item wow fadeIn space-y-2';
-    // archiveElement.href = item.pageUrl;
     archiveElement.innerHTML = `
-    
-    <div class="flex  justify-center relative overflow-hidden group cursor-pointer border rounded-xl"
-    >
-        
-        <img 
-            src="${item.imgurl}" 
-            alt="${item.title}"
-            class="w-full h-full object-cover"
-        />
-
+    <div class="flex justify-center relative overflow-hidden group cursor-pointer border rounded-xl">
+        <img src="${item.imgurl}" alt="${item.title}" class="w-full h-full object-cover" />
         <div class="video-play-button">
-            <i 
-                class="fa-solid fa-play text-white text-4xl">
-            </i>
+            <i class="fa-solid fa-play text-white text-4xl"></i>
         </div>
-
         <div class="video-link">
-            <a 
-                class="popup-video"
-                href="${item.videourl}"
-            >
-            </a>
+            <a class="popup-video" href="${item.videourl}"></a>
         </div>
     </div>
     <p class="text-base">${item.title}</p>
     `;
-
     const playButton = archiveElement.querySelector('.video-play-button');
     playButton.addEventListener('click', function() {
         $(archiveElement.querySelector('.popup-video')).magnificPopup('open');
     });
-
-    archiveElement
-    .querySelector('.status')
-    // .addEventListener('click');
     return archiveElement;
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     fetch('./js/archive.json')
     .then(response => {
-        if (!response.ok){
+        if (!response.ok) {
             throw new Error('Network response not ok');
         }
         return response.json()
@@ -69,10 +48,8 @@ document.addEventListener("DOMContentLoaded", function() {
             archiveElements.push(archiveElement);
             archiveWrapper.appendChild(archiveElement);
         });
-
-        initializeMagnificPopup()
-       
-        
+        initializeMagnificPopup();
+        applyFiltersFromURL();  
     })
     .catch(error => console.error('Error fetching data:', error));
 });
@@ -87,52 +64,50 @@ function initializeMagnificPopup() {
     });
 }
 
-
-filtersContainer.addEventListener('change', filterArchive);
-searchInput.addEventListener('input', filterArchive);
-
-locationFiltersContainer.addEventListener('change', filterArchive);
-searchInput.addEventListener('input', filterArchive);
-
-tagsFiltersContainer.addEventListener('change', filterArchive);
-searchInput.addEventListener('input', filterArchive);
-
+filtersContainer.addEventListener('change', () => {
+    filterArchive();
+    updateURLWithFilters();
+});
+searchInput.addEventListener('input', () => {
+    filterArchive();
+    updateURLWithFilters();
+});
+locationFiltersContainer.addEventListener('change', () => {
+    filterArchive();
+    updateURLWithFilters();
+});
+tagsFiltersContainer.addEventListener('change', () => {
+    filterArchive();
+    updateURLWithFilters();
+});
 
 function filterArchive() {
     const searchTerm = searchInput.value.trim().toLowerCase();
-  
     const checkedCategories = Array.from(categoryCheckBoxes)
         .filter((catCheck) => catCheck.checked)
         .map((catCheck) => catCheck.id);
-  
     const checkedLocations = Array.from(locationCheckBoxes)
         .filter((locCheck) => locCheck.checked)
         .map((locCheck) => locCheck.id);
-
     const checkedTags = Array.from(tagsCheckBoxes)
         .filter((tagsCheck) => tagsCheck.checked)
         .map((tagsCheck) => tagsCheck.id);
   
-    archiveElements.forEach((archiveElement) => {
-        const index = archiveElements.indexOf(archiveElement);
-        if (index >= 0 && index < archive.length) {
-            const archiveItem = archive[index]; 
-  
-            const matchesSearchTerm = archiveItem.title.toLowerCase().includes(searchTerm);
-            const isInCheckedCategory = checkedCategories.length === 0 || checkedCategories.includes(archiveItem.category);
-            const isInCheckedLocation = checkedLocations.length === 0 || checkedLocations.includes(archiveItem.location.toLowerCase());
-            const isInCheckedTags = checkedTags.length === 0 || checkedTags.includes(archiveItem.tag);
-  
-            if (matchesSearchTerm && isInCheckedCategory && isInCheckedLocation &&isInCheckedTags) {
-                archiveElement.classList.remove('hidden');
-            } else {
-                archiveElement.classList.add('hidden');
-            }
+    archiveElements.forEach((archiveElement, index) => {
+        const archiveItem = archive[index]; 
+        const matchesSearchTerm = archiveItem.title.toLowerCase().includes(searchTerm);
+        const isInCheckedCategory = checkedCategories.length === 0 || checkedCategories.includes(archiveItem.category);
+        const isInCheckedLocation = checkedLocations.length === 0 || checkedLocations.includes(archiveItem.location.toLowerCase());
+        const isInCheckedTags = checkedTags.length === 0 || checkedTags.includes(archiveItem.tag);
+
+        if (matchesSearchTerm && isInCheckedCategory && isInCheckedLocation && isInCheckedTags) {
+            archiveElement.classList.remove('hidden');
+        } else {
+            archiveElement.classList.add('hidden');
         }
     });
     initializeMagnificPopup();
 }
-
 
 function updateURLWithFilters() {
     const searchTerm = encodeURIComponent(searchInput.value.trim().toLowerCase());
@@ -148,34 +123,17 @@ function updateURLWithFilters() {
     
     const params = new URLSearchParams();
     params.append('filter', 'on');
-    params.append('search', searchTerm);
+    if (searchTerm) params.append('search', searchTerm);
     checkedCategories.forEach(category => params.append('category', category));
     checkedLocations.forEach(location => params.append('location', location));
     checkedTags.forEach(tag => params.append('tag', tag));
     
-    
     const baseURL = window.location.origin + window.location.pathname;
-    const filterURL = `${baseURL}?${params.toString()}`; 
+    const filterURL = `${baseURL}?${params.toString()}`;
     
+    console.log('Constructed URL:', filterURL); 
     window.history.replaceState({}, '', filterURL); 
 }
-
-filtersContainer.addEventListener('change', () => {
-    filterArchive();
-    updateURLWithFilters(); 
-});
-searchInput.addEventListener('input', () => {
-    filterArchive();
-    updateURLWithFilters(); 
-});
-locationFiltersContainer.addEventListener('change', () => {
-    filterArchive();
-    updateURLWithFilters(); 
-});
-tagsFiltersContainer.addEventListener('change', () => {
-    filterArchive();
-    updateURLWithFilters(); 
-});
 
 function applyFiltersFromURL() {
     const urlParams = new URLSearchParams(window.location.search);
@@ -184,16 +142,19 @@ function applyFiltersFromURL() {
     searchInput.value = searchTerm || '';
 
     const categories = urlParams.getAll('category');
+    console.log('Categories:', categories); 
     categoryCheckBoxes.forEach(checkbox => {
         checkbox.checked = categories.includes(checkbox.id);
     });
 
     const locations = urlParams.getAll('location');
+    console.log('Locations:', locations); 
     locationCheckBoxes.forEach(checkbox => {
         checkbox.checked = locations.includes(checkbox.id);
     });
 
     const tags = urlParams.getAll('tag');
+    console.log('Tags:', tags); 
     tagsCheckBoxes.forEach(checkbox => {
         checkbox.checked = tags.includes(checkbox.id);
     });
@@ -204,11 +165,3 @@ function applyFiltersFromURL() {
 window.addEventListener('DOMContentLoaded', () => {
     applyFiltersFromURL();
 });
-
-
-
-// $('.popup-video').magnificPopup({
-
-//     type: 'iframe'
-
-// });
